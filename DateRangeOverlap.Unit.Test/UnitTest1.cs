0659ff3a581ac0fb51fb447ead62ac7079019b06
Rubén Carretero <ruben.carretero@wolterskluwer.com>
2021-06-02 16:30:58 +0200

    Initial commit

---
+++ DateRangeOverlap.Unit.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+
+namespace DateRangeOverlap.Unit.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Range_one_is_on_range_two()
+        {
+
+        }
+
+        [Fact]
+        public void Range_one_ends_before_range_two_starts()
+        {
+        }
+
+        [Fact]
+        public void Range_one_starts_after_range_two_ends()
+        {
+        }
+    }
+}
